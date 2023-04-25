59eadf14d3d3351f9b70e751a1e5d47824cb749b
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-04-25 10:48:48 +0700

    add logout api

---
+++ server-template/controllers/logoutController.js
@@ -0,0 +1,24 @@
+const User = require("../model/User");
+
+const handleLogout = async (req, res) => {
+  const cookies = req?.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204);
+  const refreshToken = cookies.jwt;
+
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  if (!foundUser) {
+    // remove refreshToken from cookie
+    res.clearCookie("jwt", { httpOnly: true });
+    return res.sendStatus(204);
+  }
+
+  // remove refreshToken from database
+  foundUser.refreshToken = "";
+  await foundUser.save();
+
+  // remove refreshToken from cookie
+  res.clearCookie("jwt", { httpOnly: true });
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
